fccc0464b3b2be08c2105e4e3fab702403e9ea01
Phil Rinaldi <pg3rinaldi@gmail.com>
2021-11-01 15:26:54 -0500

    ground state energy with mps

--- ground_state.py
+++ ground_state.py
@@ -7,18 +7,22 @@ from quspin.operators import hamiltonian
 from quspin.basis import spinful_fermion_basis_1d
 from time import time
 from tools import Parameters
+from tenpy.simulations.ground_state_search import GroundStateSearch
+from tenpy.models.hubbard import FermiHubbardModel
+
 
 """Model parameters"""
-nsites = 10
+nsites = 20
 t0 = 0.52
 u = 1 * t0
 bc = "periodic"
 
 params = Parameters(nsites, u, t0, bc)
-
+'''
 """Exact calculation of ground state using QuSpin"""
 ti = time()
 basis = spinful_fermion_basis_1d(L=params.nsites, Nf=(params.nup, params.ndown))
+
 
 # defines onsite coupling
 onsite_interaction = [[1., site, site] for site in range(nsites)]
@@ -42,8 +46,25 @@ hop_right = hop_left.getH()
 """This is the system hamiltonian"""
 ham = -params.t0 * (hop_left + hop_right) + params.u * onsite
 
+
 """Ground state is the eigenstate of the hamiltonian corresponding to the lowest eigenergy"""
 (exact_e,), exact_ground = ham.eigsh(k=1, which='SA')
 tf = time()
 print("Exact energy of the ground state with U = {}t0 is {}".format(params.u, exact_e))
 print("It took", tf - ti, "seconds to calculate")
+'''
+
+
+
+"""
+---------------------------------------
+Simulate ground state energy with tenpy
+---------------------------------------
+"""
+model_params = dict(cons_N='N', cons_Sz='Sz', L=params.nsites, t=params.t0,U=params.u)
+search = GroundStateSearch(dict(model_class=FermiHubbardModel, model_params=model_params, initial_state_params=dict(method="mps_product_state", product_state=["up", "down"]*(params.nsites//2))))
+start = time()
+results = search.run()
+end = time()
+print("Ground state energy: {}".format(results["energy"]))
+print("Runtime: {} seconds".format( (end-start) ))
